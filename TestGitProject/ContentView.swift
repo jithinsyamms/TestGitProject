77b498fe6808d931ae3b3a17a6e003d23059d0c9
jithinsyamms <jithinsyamms@gmail.com>
2021-12-23 13:51:06 +0530

    Update ContentView.swift

--- TestGitProject/ContentView.swift
+++ TestGitProject/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Text("Hello, world!")
             .padding()
+            Text("Ok Boss")
+        }
+        
     }
 }
 
